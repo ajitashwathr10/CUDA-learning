02801c56afcc41ca775cec6081ed4ae6280f05d4
Ajit Ashwath <141044380+ajitashwathr10@users.noreply.github.com>
2025-02-26 13:42:36 +0530

    Add files via upload
    Result:
    Running in FUNCTIONAL mode...
    Compiling...
    Executing...
    Hello World (from CPU)!
    Hello World from Thread 627467593 in Block -587216448!
    Hello World from Thread 627467593 in Block -587200976!
    Hello World from Thread 627467593 in Block 0!
    Hello World from Thread 627467593 in Block 0!
    Hello World from Thread 627467593 in Block -587216448!
    Hello World from Thread 627467593 in Block 0!
    Hello World from Thread 627467593 in Block 0!
    Hello World from Thread 627467593 in Block 0!
    Goodbye World (from CPU)!
    Exit status: 0

---
+++ Hello World.cu
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+__global__ void HelloWorld() {
+    printf("Hello World from Thread %d in Block %d!\n", threadIdx.x, blockIdx.x);
+}
+
+int main() {
+    printf("Hello World (from CPU)!\n");
+    
+    HelloWorld<<<2, 4>>>();
+    //Launching kernel with 2 blocks (with 4 threads each)
+    cudaDeviceSynchronize();
+    //Waiting for GPU to finish
+    cudaError_t error = cudaGetLastError();
+    if(error != cudaSuccess) {
+        fprintf(stderr, "ERROR: %s\n", cudaGetErrorString(error));
+        return -1;
+    }
+    printf("Goodbye World (from CPU)!\n"); // Added newline character
+    return 0;
+}
